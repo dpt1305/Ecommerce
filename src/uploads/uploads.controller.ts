8b993173f8a6b7bd819852d9b22e84799432f507
tungjj <tungplatin@gmail.com>
2022-04-26 10:31:55 +0700

    DONE: upload

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,5 +1,5 @@
 import { ApiTags, ApiConsumes, ApiBody } from '@nestjs/swagger';
-import { UploadsService } from './uploads.service';
+// import { UploadsService } from './uploads.service';
 import {
   Controller,
   Get,
@@ -11,12 +11,18 @@ import {
   UploadedFile,
   UploadedFiles,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path';
+import {
+  FileInterceptor,
+  FilesInterceptor,
+  FileFieldsInterceptor,
+  AnyFilesInterceptor,
+} from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 @ApiTags('File')
 @Controller('uploads')
 export class UploadsController {
-  constructor(private uploadsService: UploadsService) {}
+  // constructor(private uploadsService: UploadsService) {}
 
   @Post('file')
   @UseInterceptors(
@@ -26,12 +32,15 @@ export class UploadsController {
         filename: function (req, file, cb) {
           const uniqueSuffix =
             Date.now() + '-' + Math.round(Math.random() * 1e9);
-          cb(
-            null,
-            file.fieldname + '-' + uniqueSuffix + '-' + file.originalname,
-          );
+          cb(null, uniqueSuffix + '-' + file.originalname);
         },
       }),
+      fileFilter: function (req, file, callback) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+          return callback(new Error('Only image files are allowed!'), false);
+        }
+        callback(null, true);
+      },
     }),
   )
   @ApiConsumes('multipart/form-data')
@@ -52,14 +61,32 @@ export class UploadsController {
       file,
     };
   }
-
   @Post('files')
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      storage: diskStorage({
+        destination: './files',
+        filename: function (req, file, cb) {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, uniqueSuffix + '-' + file.originalname);
+        },
+      }),
+      fileFilter: function (req, file, callback) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+          return callback(new Error('Only image files are allowed!'), false);
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  // @UseInterceptors(FileFieldsInterceptor([{ name: 'images', maxCount: 2 }]))
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
       type: 'object',
       properties: {
-        file: {
+        files: {
           type: 'array',
           items: {
             type: 'string',
@@ -69,22 +96,69 @@ export class UploadsController {
       },
     },
   })
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './images',
-        filename: function (req, file, cb) {
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-          cb(
-            null,
-            file.fieldname + '-' + uniqueSuffix + '-' + file.originalname,
-          );
-        },
-      }),
-    }),
-  )
-  uploadManyImages(@UploadedFiles() files) {
+  uploadImages(@UploadedFiles() files: Array<Express.Multer.File>) {
     console.log(files);
+    return {
+      files,
+    };
   }
+
+  // @ApiConsumes('multipart/form-data')
+  // @ApiBody({
+  //   schema: {
+  //     type: 'object',
+  //     properties: {
+  //       file: {
+  //         type: 'array',
+  //         items: {
+  //           type: 'string',
+  //           format: 'binary',
+  //         },
+  //       },
+  //     },
+  //   },
+  // })
+  // @Post('files')
+  // @UseInterceptors(
+  //   FileInterceptor('image', {
+  //     storage: diskStorage({
+  //       destination: './images',
+  //       filename: editFileName,
+  //     }),
+  //     fileFilter: imageFileFilter,
+  //   }),
+  // )
+  // uploadManyImages(@UploadedFile() files) {
+  //   console.log(files);
+  //   const response = [];
+  //   files.forEach((file) => {
+  //     const fileReponse = {
+  //       originalname: file.originalname,
+  //       filename: file.filename,
+  //     };
+  //     response.push(fileReponse);
+  //   });
+  //   return response;
+  // }
 }
+export const editName = function (req, file, cb) {
+  const uniqueSuffix =
+    Date.now() + '-' + Math.round(Math.random() * 1e9);
+  cb(null, uniqueSuffix + '-' + file.originalname);
+};
+export const editFileName = (req, file, callback) => {
+  const name = file.originalname.split('.')[0];
+  const fileExtName = path.extname(file.originalname);
+  const randomName = Array(4)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+  callback(null, `${name}-${randomName}${fileExtName}`);
+};
+
+export const imageFileFilter = function(req, file, callback) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new Error('Only image files are allowed!'), false);
+  }
+  callback(null, true);
+};
--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UploadsService } from './uploads.service';
 import { UploadsController } from './uploads.controller';
 
 @Module({
-  providers: [UploadsService],
+  // providers: [UploadsService],
   controllers: [UploadsController],
 })
 export class UploadsModule {}
